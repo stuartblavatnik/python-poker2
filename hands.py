3e115cdfe98476514f1d6b1a00fca3e6699b6adf
stuartblavatnik <stuartb@twoforboth.com>
2016-05-30 16:43:01 -0400

    Completed parsing tests

--- hands.py
+++ hands.py
@@ -24,36 +24,36 @@ STRAIGHT_FLUSH_DESC = " Straight flush"
 
 
 class Hands:
-
     cards = []
     type = ""
     threeOfAKind = None
     fourOfAKind = None
     pairs = []
+    baby_straight = False
 
-    def __init__(self, cards):
-        self.cards = cards
+    def __init__(self, _cards):
+        self.cards = _cards
         self.type = self.parse()
 
-#    def compare(self, other):
+    #    def compare(self, other):
 
     def __build_multiples(self, ranks):
 
         multiple = False
         rank_keys = Counter(ranks).keys()
-        rankValues = list(Counter(ranks).values())
+        rank_values = list(Counter(ranks).values())
         self.pairs = []
 
-        arrayCount = len(rank_keys)
+        array_count = len(rank_keys)
 
-        for i in range(0, arrayCount):
-            if (rankValues[i] > 1):
+        for i in range(0, array_count):
+            if rank_values[i] > 1:
                 multiple = True
-                if (rankValues[i] == 2):
+                if rank_values[i] == 2:
                     self.pairs.append(list(rank_keys)[i])
-                elif (rankValues[i] == 3):
+                elif rank_values[i] == 3:
                     self.threeOfAKind = list(rank_keys)[i]
-                elif (rankValues[i] == 4):
+                elif rank_values[i] == 4:
                     self.fourOfAKind = list(rank_keys)[i]
         return multiple
 
@@ -63,23 +63,27 @@ class Hands:
 
         rank_keys = Counter(ranks).keys()
 
-        if len(rank_keys) == 5 and list(rank_keys)[4] - list(rank_keys)[0] == 4:
-            straight = True
+        if len(rank_keys) == 5:
+            if list(rank_keys)[4] - list(rank_keys)[0] == 4:
+                straight = True
+            elif list(rank_keys)[0] == 0 and list(rank_keys)[1] == 1 and list(rank_keys)[2] == 2 and \
+                list(rank_keys)[3] == 3 and list(rank_keys)[4] == 12:
+                straight = True
+                self.baby_straight = True
 
         return straight
 
     def parse(self):
 
-        HandDescription = namedtuple('HandDescription', 'description extraDescription extra')
+        hand_description = namedtuple('HandDescription', 'description extraDescription extra')
 
         multiple = False
         flush = False
 
         self.cards.sort()
-        highCard = self.cards[-1]
-        highCardDescription = cards.getSingleCardDescription(highCard) + " " + "High"
+        high_card = self.cards[-1]
+        high_card_description = cards.getSingleCardDescription(high_card) + " " + "High"
 
-        #Build arrays to hold suits and ranks
         suits = []
         ranks = []
 
@@ -87,65 +91,73 @@ class Hands:
             suits.append(cards.getSuit(card))
             ranks.append(cards.getRank(card))
 
-        #Normalize suits to get the count of different suits in the hand
+        # Normalize suits to get the count of different suits in the hand
         suits = list(set(suits))
 
         if len(suits) == 1:
             flush = True
             flushType = suits[0]
-        else :
+        else:
             multiple = self.__build_multiples(ranks)
 
-        if not multiple :
+        if not multiple:
 
             straight = self.__is_straight(ranks)
 
-            if straight == True and flush == True:
-                HandDescription.description = STRAIGHT_FLUSH
-                HandDescription.extraDescription = highCardDescription
-                HandDescription.extra = highCard
-            elif flush == True:
-                HandDescription.description = FLUSH
-                HandDescription.extraDescription = highCardDescription
-                HandDescription.extra = highCard
-            elif straight == True:
-                HandDescription.description = STRAIGHT
-                HandDescription.extraDescription = highCardDescription
-                HandDescription.extra = highCard
+            if straight and flush:
+                hand_description.description = STRAIGHT_FLUSH
+
+                if self.baby_straight:
+                    hand_description.extraDescription = "Five High"
+                    hand_description.extra = self.cards[3]
+                else:
+                    hand_description.extraDescription = high_card_description
+                    hand_description.extra = high_card
+            elif flush:
+                hand_description.description = FLUSH
+                hand_description.extraDescription = high_card_description
+                hand_description.extra = high_card
+            elif straight:
+                hand_description.description = STRAIGHT
+
+                if self.baby_straight:
+                    hand_description.extraDescription = "Five High"
+                    hand_description.extra = self.cards[3]
+                else:
+                    hand_description.extraDescription = high_card_description
+                    hand_description.extra = high_card
             else:
-                HandDescription.description = HIGH_CARD
-                HandDescription.extraDescription = highCardDescription
-                HandDescription.extra = highCard
+                hand_description.description = HIGH_CARD
+                hand_description.extraDescription = high_card_description
+                hand_description.extra = high_card
 
         elif self.fourOfAKind is not None:
-            HandDescription.description = FOUR_OF_A_KIND
-            HandDescription.extraDescription = cards.getPluralRankDescription(self.fourOfAKind)
-            HandDescription.extra = self.fourOfAKind
+            hand_description.description = FOUR_OF_A_KIND
+            hand_description.extraDescription = cards.getPluralRankDescription(self.fourOfAKind)
+            hand_description.extra = self.fourOfAKind
         elif self.threeOfAKind is not None and len(self.pairs) == 1:
-            HandDescription.description = FULL_HOUSE
-            HandDescription.extraDescription = cards.getPluralRankDescription(self.threeOfAKind) + " over " + \
-                                               cards.getPluralRankDescription(self.pairs[0])
-            HandDescription.extra = self.threeOfAKind
+            hand_description.description = FULL_HOUSE
+            hand_description.extraDescription = cards.getPluralRankDescription(self.threeOfAKind) + " over " + \
+                                                cards.getPluralRankDescription(self.pairs[0])
+            hand_description.extra = self.threeOfAKind
         elif self.threeOfAKind is not None:
-            HandDescription.description = THREE_OF_A_KIND
-            HandDescription.extraDescription = cards.getPluralRankDescription(self.threeOfAKind)
-            HandDescription.extra = self.threeOfAKind
+            hand_description.description = THREE_OF_A_KIND
+            hand_description.extraDescription = cards.getPluralRankDescription(self.threeOfAKind)
+            hand_description.extra = self.threeOfAKind
         elif self.pairs is not None and len(self.pairs) == 2:
             self.pairs.sort()
-            HandDescription.description = TWO_PAIR
-            HandDescription.extraDescription = cards.getPluralRankDescription(self.pairs[1]) + " over " + \
-                cards.getPluralRankDescription(self.pairs[0])
-            HandDescription.extra = str(self.pairs[1]) + " " + str(self.pairs[0])
+            hand_description.description = TWO_PAIR
+            hand_description.extraDescription = cards.getPluralRankDescription(self.pairs[1]) + " over " + \
+                                                cards.getPluralRankDescription(self.pairs[0])
+            hand_description.extra = str(self.pairs[1]) + " " + str(self.pairs[0])
         elif self.pairs is not None:
-            HandDescription.description = PAIR
-            HandDescription.extraDescription = cards.getPluralRankDescription(self.pairs[0])
-            HandDescription.extra = self.pairs[0]
+            hand_description.description = PAIR
+            hand_description.extraDescription = cards.getPluralRankDescription(self.pairs[0])
+            hand_description.extra = self.pairs[0]
 
-        return HandDescription
+        return hand_description
 
-
-
-    def getdescription(self, handType, extra):
+    def get_description(self, handType, extra):
 
         if handType == STRAIGHT_FLUSH:
             description = extra + STRAIGHT_FLUSH_DESC
@@ -172,30 +184,12 @@ class Hands:
 
         ranks = []
 
-        for i in 0,5:
+        for i in 0, 5:
             ranks.append(self.cards[i])
 
         return ranks
 
     def display(self):
 
-        for i in 0,5:
+        for i in 0, 5:
             print(cards.getCardDescription(self.cards[i]) + "\n")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- tests/test_hands.py
+++ tests/test_hands.py
@@ -1,14 +1,11 @@
 from unittest import TestCase
-
 import hands
 
-import cards
 
 class TestHands(TestCase):
-
     def doParseTest(self, cards, expected_description, expected_extra_description, expected_extra):
-        cardsinhand = cards
-        hand = hands.Hands(cardsinhand)
+        cards_in_hand = cards
+        hand = hands.Hands(cards_in_hand)
 
         hand_description_tuple = hand.parse()
 
@@ -16,28 +13,21 @@ class TestHands(TestCase):
         self.assertEquals(expected_extra_description, hand_description_tuple.extraDescription)
         self.assertEquals(expected_extra, hand_description_tuple.extra)
 
-    def test_parse_four_of_a_kind(self):
-
-        self.doParseTest([0,1,2,3,4], hands.FOUR_OF_A_KIND, "Twos", 0)
-        self.doParseTest([48, 49, 50, 51, 4], hands.FOUR_OF_A_KIND, "Aces", 12)
-
-    def test_parse_three_of_a_kind(self):
-
-        self.doParseTest([0, 1, 2, 20, 40], hands.THREE_OF_A_KIND, "Twos", 0)
-        self.doParseTest([4, 5, 6, 18, 30], hands.THREE_OF_A_KIND, "Threes", 1)
-
-    def test_parse_two_pairs(self):
-
-        self.doParseTest([0, 1, 4, 5, 40], hands.TWO_PAIR, "Threes over Twos", "1 0")
-        self.doParseTest([4, 48, 49, 5, 27], hands.TWO_PAIR, "Aces over Threes", "12 1")
+    def test_parse_high_card(self):
+        self.doParseTest([40, 0, 18, 51, 27], hands.HIGH_CARD, "Ace High", 51)
+        self.doParseTest([47, 0, 18, 10, 26], hands.HIGH_CARD, "King High", 47)
 
     def test_parse_pair(self):
         self.doParseTest([40, 0, 18, 51, 1], hands.PAIR, "Twos", 0)
         self.doParseTest([47, 0, 18, 51, 46], hands.PAIR, "Kings", 11)
 
-    def test_parse_flush(self):
-        self.doParseTest([0, 4, 8, 16, 32], hands.FLUSH, "Ten High", 32)
-        self.doParseTest([37, 9, 17, 5, 1], hands.FLUSH, "Jack High", 37)
+    def test_parse_two_pairs(self):
+        self.doParseTest([0, 1, 4, 5, 40], hands.TWO_PAIR, "Threes over Twos", "1 0")
+        self.doParseTest([4, 48, 49, 5, 27], hands.TWO_PAIR, "Aces over Threes", "12 1")
+
+    def test_parse_three_of_a_kind(self):
+        self.doParseTest([0, 1, 2, 20, 40], hands.THREE_OF_A_KIND, "Twos", 0)
+        self.doParseTest([4, 5, 6, 18, 30], hands.THREE_OF_A_KIND, "Threes", 1)
 
     def test_parse_straight(self):
         self.doParseTest([0, 5, 8, 12, 16], hands.STRAIGHT, "Six High", 16)
@@ -45,9 +35,22 @@ class TestHands(TestCase):
         self.doParseTest([51, 46, 42, 38, 34], hands.STRAIGHT, "Ace High", 51)
         self.doParseTest([51, 0, 4, 8, 12], hands.STRAIGHT, "Five High", 12)
 
+    def test_parse_flush(self):
+        self.doParseTest([0, 4, 8, 16, 32], hands.FLUSH, "Ten High", 32)
+        self.doParseTest([37, 9, 17, 5, 1], hands.FLUSH, "Jack High", 37)
+
+    def test_parse_full_house(self):
+        self.doParseTest([0, 1, 2, 51, 50], hands.FULL_HOUSE, "Twos over Aces", 0)
+        self.doParseTest([22, 4, 5, 6, 23], hands.FULL_HOUSE, "Threes over Sevens", 1)
+
+    def test_parse_four_of_a_kind(self):
+        self.doParseTest([0, 1, 2, 3, 4], hands.FOUR_OF_A_KIND, "Twos", 0)
+        self.doParseTest([48, 49, 50, 51, 4], hands.FOUR_OF_A_KIND, "Aces", 12)
 
     def test_parse_straight_flush(self):
         self.doParseTest([0, 4, 8, 12, 16], hands.STRAIGHT_FLUSH, "Six High", 16)
+        self.doParseTest([51, 47, 43, 39, 35], hands.STRAIGHT_FLUSH, "Ace High", 51)
+        self.doParseTest([51, 3, 7, 11, 15], hands.STRAIGHT_FLUSH, "Five High", 15)
 
 '''
     def test_getdescription(self):
