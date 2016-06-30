cba2b40a5f863a1b4109bafe6a233ffb2a7d723d
stuartb <stuart.blavatnik@pegs.com>
2016-06-30 17:51:02 -0400

    Added comments.

--- cards.py
+++ cards.py
@@ -1,11 +1,38 @@
 '''
-Methods that operate on cards
+Methods that operate on cards.  The deck is made up of values from 0-51 with the individual index of the card used to
+describe it's rank and suit.
+
+Rank is defined as index / 4
+
+Examples:
+
+    cardIndex = 3
+    3/4 = 0
+    ranks[0]  =    "Two"
+
+    cardIndex = 50
+    50/4 = 12
+    ranks[12]  =   "Ace"
+
+Suit is defined as index % 4   -- The arbitrary order of suits is Clubs, Diamonds, Hearts and Spades
+
 '''
 import math
 
+'''
+Descriptions of the cards.
+'''
 ranks = ("Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace")
+
+'''
+Plural descriptions of the cards.
+'''
 pluralRanks = (
 "Twos", "Threes", "Fours", "Fives", "Sixes", "Sevens", "Eights", "Nines", "Tens", "Jacks", "Queens", "Kings", "Aces")
+
+'''
+Description of suits
+'''
 suits = ("Clubs", "Diamonds", "Hearts", "Spades")
 
 
